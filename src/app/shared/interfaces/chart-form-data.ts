e41880105569ed8bfac3fb758fec4473802f438b
Abhinav Goel <abhinavgo1000@gmail.com>
2025-02-08 20:20:06 -0700

    interface for chart form data configured

--- src/app/shared/interfaces/chart-form-data.ts
+++ src/app/shared/interfaces/chart-form-data.ts
@@ -1,2 +1,13 @@
 export interface ChartFormData {
+    id: number;
+    name: string;
+    email: string;
+    telephone: string;
+    dateOfBirth: string;
+    dueDate: string;
+    numberOfUnits: number;
+    unitPrice: number;
+    productUse: string;
+    productRating: number;
+    comments: string;
 }
--- src/app/shared/services/form-data-entry.service.ts
+++ src/app/shared/services/form-data-entry.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ChartFormData } from '../interfaces/chart-form-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormDataEntryService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
 }
