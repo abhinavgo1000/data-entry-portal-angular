4d010a11f17366e8bc939310a728ac19ff10767d
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-14 17:03:59 -0600

    edit and delete services configured

--- src/app/shared/services/chart-data-delete.service.ts
+++ src/app/shared/services/chart-data-delete.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { ChartFormData } from '../interfaces/chart-form-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartDataDeleteService {
 
-  constructor() { }
+  readonly apiUrl = 'http://localhost:5000/api/form/delete-form-data'; 
+
+  constructor(private http: HttpClient) { }
+
+  deleteChartData(id: string): Observable<ChartFormData> {
+    return this.http.delete<ChartFormData>(`${this.apiUrl}/${id}`).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error deleting chart data', error);
+        throw error;
+      })
+    );
+  }
 }
--- src/app/shared/services/chart-data-edit.service.ts
+++ src/app/shared/services/chart-data-edit.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { ChartFormData } from '../interfaces/chart-form-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartDataEditService {
 
-  constructor() { }
+  readonly apiUrl = 'http://localhost:5000/api/form/update-form-data'; 
+
+  constructor(private http: HttpClient) { }
+
+  updateChartData(formData: ChartFormData): Observable<ChartFormData> {
+    return this.http.put<ChartFormData>(`${this.apiUrl}/${formData._id}`, formData).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error updating chart data', error);
+        throw error;
+      })
+    );
+  }
 }
