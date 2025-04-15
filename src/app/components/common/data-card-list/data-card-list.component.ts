b959117a4937d1f6194ebc034ec52f495c5bdca4
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-15 15:08:01 -0600

    paginator configured

--- src/app/components/common/data-card-list/data-card-list.component.ts
+++ src/app/components/common/data-card-list/data-card-list.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { PageEvent, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 
 import { ChartDataReadService, ChartDataDeleteService } from 'services';
 import { ChartFormData } from 'interfaces';
 import { DateFormatterPipe, TelephoneFormatterPipe } from 'pipes';
+
+@Injectable()
+export class PaginatorIntl extends MatPaginatorIntl {
+  override getRangeLabel = (page: number, pageSize: number, length: number) => {
+    return `Page ${page + 1} of ${Math.ceil(length/pageSize)}`
+  }
+}
 
 @Component({
   selector: 'app-data-card-list',
@@ -16,16 +24,33 @@ import { DateFormatterPipe, TelephoneFormatterPipe } from 'pipes';
     MatIconModule,
     MatButtonModule,
     MatCardModule,
+    MatPaginatorModule,
     DateFormatterPipe,
     TelephoneFormatterPipe
   ],
-  providers: [DatePipe],
+  providers: [
+    DatePipe, 
+    { provide: MatPaginatorIntl, useClass: PaginatorIntl }
+  ],
   templateUrl: './data-card-list.component.html',
   styleUrl: './data-card-list.component.scss'
 })
 export class DataCardListComponent implements OnInit {
 
   cardsData: ChartFormData[] = [];
+  paginatedCards: ChartFormData[] = [];
+
+  length = this.cardsData.length;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent!: PageEvent;
 
   constructor(
     private _router: Router,
@@ -35,6 +60,8 @@ export class DataCardListComponent implements OnInit {
   ngOnInit(): void {
     this._chartService.fetchChartData().subscribe((data: ChartFormData[]) => {
       this.cardsData = data;
+      this.length = data.length;
+      this.updatePaginatedCards();
     });
   }
 
@@ -46,5 +73,19 @@ export class DataCardListComponent implements OnInit {
     this._deleteService.deleteChartData(card._id).subscribe(() => {
       this.cardsData = this.cardsData.filter(item => item._id !== card._id);
     });
+  }
+
+  updatePaginatedCards(): void {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedCards = this.cardsData.slice(startIndex, endIndex);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    this.updatePaginatedCards();
   }
 }
